d22006ef4ae32194098291a1bd36f83b22ebf6af
Nick Ramsey <nramseysc@gmail.com>
2019-08-02 14:48:07 -0700

    initial Volunteer class methods and passing specs

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,3 +1,18 @@
 class Volunteer
+  attr_accessor :name, :project_id
+  attr_reader :id
 
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+
+  def ==(volunteer_to_copmare)
+    if volunteer_to_copmare != nil
+      (self.name() == volunteer_to_copmare.name()) && (self.project_id() == volunteer_to_copmare.project_id())
+    else
+      false
+    end
+  end  
 end
