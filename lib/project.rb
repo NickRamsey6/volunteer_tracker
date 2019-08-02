8bdacbba5f8fc81e08cb9db832cdf2759dee0fc1
Nick Ramsey <nramseysc@gmail.com>
2019-08-02 15:17:38 -0700

    complete unit test coverage

--- lib/project.rb
+++ lib/project.rb
@@ -47,4 +47,8 @@ class Project
     DB.exec("DELETE FROM projects WHERE id = #{@id};")
   end
 
+  def volunteers
+    Volunteer.find_by_project(self.id)
+  end
+
 end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -32,4 +32,27 @@ class Volunteer
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{self.name}', #{self.project_id}) RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
+
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    if volunteer
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      Volunteer.new({:name => name, :project_id => project_id, :id => id})
+    else
+      nil
+    end
+  end
+
+  def self.find_by_project(project_id)
+    volunteers = []
+    returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{project_id};")
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
 end
