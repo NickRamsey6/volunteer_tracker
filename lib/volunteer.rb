4ae252c27f8311745466d3656760c4d5b63727a1
Nick Ramsey <nramseysc@gmail.com>
2019-08-02 15:00:32 -0700

    add full Volunteer class functionality

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -14,5 +14,22 @@ class Volunteer
     else
       false
     end
-  end  
+  end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * from volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{self.name}', #{self.project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
 end
